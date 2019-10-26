11d6425b2f2160a6eb52f7cbe8c09e751db27a22
Adam Alenius <adam.alenius@konsult.folksam.se>
2019-10-26 12:56:04 +0200

    feat: split list item into separate component and add styling for when the item is selected or not

---
+++ src/components/ListItem.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ListItemRoot = styled.div`
+  padding: 15px;
+`;
+
+const ListItemText = styled.p`
+  font-family: 'Rubik', 'Roboto', 'sans-serif';
+  color: ${(props: { selected: boolean }): string =>
+    props.selected ? '#F9F871' : '#8cd881'};
+
+  border-width: 0px;
+  border-bottom-width: ${(props: { selected: boolean }): string =>
+    props.selected ? '2px' : '0px'};
+  border-style: solid;
+
+  padding: 3px;
+  font-size: 20px;
+
+  ${ListItemRoot}:hover & {
+    color: #00a9bc;
+  }
+`;
+interface ListItemProp {
+  item: string;
+  selected: boolean;
+  onClick: (item: string) => void;
+}
+
+const ListItem: React.FC<ListItemProp> = ({
+  item,
+  selected = false,
+  onClick
+}) => {
+  return (
+    <ListItemRoot onClick={(): void => onClick(item)} key={item}>
+      <ListItemText selected={selected}>{item}</ListItemText>
+    </ListItemRoot>
+  );
+};
+
+export default ListItem;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavBar } from '../components/NavBar';
 import styled from 'styled-components';
+import ListItem from '../components/ListItem';
 
 const PageRoot = styled.div`
   display: flex;
@@ -11,27 +12,79 @@ const PageRoot = styled.div`
   align-items: center;
 `;
 
-const Presentation = styled.p`
+const Presentation = styled.h1`
   /* font-family: 'Roboto', 'sans-serif'; */
   font-family: 'Rubik', 'Roboto', 'sans-serif';
   color: #8cd881;
-  font-size: 24px;
 `;
 
 export const Home: React.FC = () => {
   return (
     <PageRoot>
       <NavBar />
-      <Presentation>Vad är du sugen på idag?</Presentation>
+      <Presentation>What kind of food are you craving?</Presentation>
       <FoodTypeSelector />
     </PageRoot>
   );
 };
 
+const cusineList = ['Thai', 'Mexican', 'Swedish', 'African'];
+
+const SelectorWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  background-color: #454754;
+  padding: 100px;
+
+  width: 100%;
+`;
+const FoodHeader = styled.h2`
+  color: #8cd881;
+`;
+const ListWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 800px;
+`;
+
 const FoodTypeSelector: React.FC = () => {
+  const [selectedCusines, setSelectedCusines] = useState<Array<string>>([]);
+
+  const isItemSelected = (item: string): boolean => {
+    return selectedCusines.includes(item);
+  };
+
+  const onItemClick = (item: string): void => {
+    const selected = isItemSelected(item);
+    console.log(selected);
+    if (!selected) {
+      setSelectedCusines([...selectedCusines, item]);
+    } else {
+      console.log('hello');
+      const arrayWithoutItem = selectedCusines.filter(it => it !== item);
+      setSelectedCusines(arrayWithoutItem);
+    }
+  };
+
   return (
-    <div>
-      <div />
-    </div>
+    <SelectorWrapper>
+      <FoodHeader>By cusine</FoodHeader>
+      <ListWrapper>
+        {cusineList.map(it => {
+          const selected = isItemSelected(it);
+
+          return (
+            <ListItem
+              selected={selected}
+              onClick={(): void => onItemClick(it)}
+              item={it}
+              key={it}
+            />
+          );
+        })}
+      </ListWrapper>
+    </SelectorWrapper>
   );
 };
