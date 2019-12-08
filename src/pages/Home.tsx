56cbfabaca3736ccea7707d2ef26ae1b7a8a1946
Adam Alenius <adam.alenius@konsult.folksam.se>
2019-12-08 10:13:47 +0100

    feat (Home):
    collect all the views here in a single file

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,90 +1,58 @@
-import React, { useState } from 'react';
-import { NavBar } from '../components/NavBar';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import ListItem from '../components/ListItem';
+import PagePresentation from '../views/PagePresentation';
+import CuisineSelectorView from '../views/CuisineSelectorView';
+import TagsSelectorView from '../views/TagsSelectorView';
+import RecipePresentationView from '../views/RecipePresentationView';
 
 const PageRoot = styled.div`
   display: flex;
   flex-direction: column;
   flex: 1;
-  height: 100vh;
   background-color: #282c34;
   align-items: center;
 `;
 
-const Presentation = styled.h1`
-  /* font-family: 'Roboto', 'sans-serif'; */
-  font-family: 'Rubik', 'Roboto', 'sans-serif';
-  color: #8cd881;
-`;
-
 export const Home: React.FC = () => {
+  const [started, setStarted] = useState<boolean>(false);
+  const [cuisineFinished, setCuisineFinished] = useState<boolean>(false);
+  const [tagsFinished, setTagsFinished] = useState<boolean>(false);
+  const [selectedCusines, setSelectedCuisines] = useState<Array<string>>([]);
+  const [selectedTags, setSelectedTags] = useState<Array<string>>([]);
+
+  useEffect(() => {
+    if (tagsFinished === false) {
+      setSelectedTags([]);
+    }
+  }, [tagsFinished]);
+
   return (
     <PageRoot>
-      <NavBar />
-      <Presentation>What kind of food are you craving?</Presentation>
-      <FoodTypeSelector />
+      <PagePresentation started={started} setStarted={setStarted} />
+      {started ? (
+        <CuisineSelectorView
+          started={started}
+          selectedCuisines={selectedCusines}
+          setSelectedCuisines={setSelectedCuisines}
+          setCuisineFinished={setCuisineFinished}
+          setTagsFinished={setTagsFinished}
+        />
+      ) : null}
+      {started && cuisineFinished ? (
+        <TagsSelectorView
+          selectedCuisines={selectedCusines}
+          cuisineFinished={cuisineFinished}
+          selectedTags={selectedTags}
+          setSelectedTags={setSelectedTags}
+          setTagsFinished={setTagsFinished}
+        />
+      ) : null}
+      {started && cuisineFinished && tagsFinished ? (
+        <RecipePresentationView
+          selectedCuisines={selectedCusines}
+          selectedTags={selectedTags}
+        />
+      ) : null}
     </PageRoot>
-  );
-};
-
-const cusineList = ['Thai', 'Mexican', 'Swedish', 'African'];
-
-const SelectorWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  background-color: #454754;
-  padding: 100px;
-
-  width: 100%;
-`;
-const FoodHeader = styled.h2`
-  color: #8cd881;
-`;
-const ListWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  width: 800px;
-`;
-
-const FoodTypeSelector: React.FC = () => {
-  const [selectedCusines, setSelectedCusines] = useState<Array<string>>([]);
-
-  const isItemSelected = (item: string): boolean => {
-    return selectedCusines.includes(item);
-  };
-
-  const onItemClick = (item: string): void => {
-    const selected = isItemSelected(item);
-    console.log(selected);
-    if (!selected) {
-      setSelectedCusines([...selectedCusines, item]);
-    } else {
-      console.log('hello');
-      const arrayWithoutItem = selectedCusines.filter(it => it !== item);
-      setSelectedCusines(arrayWithoutItem);
-    }
-  };
-
-  return (
-    <SelectorWrapper>
-      <FoodHeader>By cusine</FoodHeader>
-      <ListWrapper>
-        {cusineList.map(it => {
-          const selected = isItemSelected(it);
-
-          return (
-            <ListItem
-              selected={selected}
-              onClick={(): void => onItemClick(it)}
-              item={it}
-              key={it}
-            />
-          );
-        })}
-      </ListWrapper>
-    </SelectorWrapper>
   );
 };
