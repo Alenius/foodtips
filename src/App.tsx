c0dd7a156990c04d58f09a8a9a79a39d84c5802c
Adam Alenius <adam.alenius@konsult.folksam.se>
2020-01-08 13:48:24 +0100

    feat (App):
    change the graphql server source to the herokuapp if deployed, otherwise localhost

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,12 @@ import { Home } from './pages/Home';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 
-const client = new ApolloClient({ uri: 'http://localhost:4000' });
+const client = new ApolloClient({
+  uri:
+    window.location.hostname === 'localhost'
+      ? 'http://localhost:4000'
+      : 'http://foodtips-server.herokuapp.com/graphql'
+});
 
 const App: React.FC = () => {
   return (
