c61747f96f2e003301703d930e27af5bec171bd6
Adam Alenius <adam.alenius@gmail.com>
2020-02-07 13:54:03 +0100

    feat: add an animation wrapper component for ease of page animations

---
+++ src/components/AnimationWrapper.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+import { useSpring, animated } from 'react-spring';
+import theme from 'theme';
+
+const MainWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: ${(props: { backgroundColor: string }) =>
+    props.backgroundColor};
+`;
+
+const AnimatedWrapper = styled(animated.div)``;
+
+interface Props {
+  backgroundColor?: string;
+}
+
+const AnimationWrapper: React.FC<Props> = ({
+  children,
+  backgroundColor = theme.primaryBackgroundColor,
+}) => {
+  const [props, set] = useSpring(() => ({ opacity: 0 }));
+
+  useEffect(() => {
+    set({ opacity: 1 });
+  }, []);
+
+  return (
+    <MainWrapper backgroundColor={backgroundColor}>
+      <AnimatedWrapper style={props}>{children}</AnimatedWrapper>
+    </MainWrapper>
+  );
+};
+
+export default AnimationWrapper;
