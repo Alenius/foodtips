50383255d1b59bc5636fe073383d78ba9a7d5e8c
Adam Alenius <adam.alenius@gmail.com>
2020-02-07 10:19:03 +0100

    style: add wrapper div for vertical alignment relative to the choices

--- src/components/NextButton.tsx
+++ src/components/NextButton.tsx
@@ -3,6 +3,12 @@ import styled from 'styled-components';
 
 import { Link } from 'react-router-dom';
 
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100%;
+`;
+
 const Button = styled(Link)`
   padding: 1rem;
   padding-left: 2rem;
@@ -12,6 +18,7 @@ const Button = styled(Link)`
   background-color: #8cd881;
   color: black;
   text-decoration: none;
+  text-align: center;
 
   :hover {
     cursor: pointer;
@@ -31,9 +38,11 @@ const NextButton: React.FC<Props> = ({
   children
 }) => {
   return (
-    <Button to={to} onClick={(): void => onClick()}>
-      {children}
-    </Button>
+    <Wrapper>
+      <Button to={to} onClick={(): void => onClick()}>
+        {children}
+      </Button>
+    </Wrapper>
   );
 };
 
