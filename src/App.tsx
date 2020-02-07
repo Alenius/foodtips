3b6860dfefd59c9487c41706e694d5ccae459683
Adam Alenius <adam.alenius@gmail.com>
2020-02-07 09:47:32 +0100

    feat: add router and routes to root component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { FoodProvider } from './context/FoodProvider';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import SelectCuisine from 'pages/SelectCuisine';
+import SelectTags from 'pages/SelectTags';
+import RecipePresentation from 'pages/RecipePresentation';
 
 const client = new ApolloClient({
   uri:
@@ -19,9 +22,10 @@ const App: React.FC = () => {
       <FoodProvider>
         <Router>
           <Switch>
-            <Route path='/'>
-              <Home />
-            </Route>
+            <Route path='/recipePresentation' component={RecipePresentation} />
+            <Route path='/selectTags' component={SelectTags} />
+            <Route path='/selectCuisine' component={SelectCuisine} />
+            <Route path='/' component={Home} />
           </Switch>
         </Router>
       </FoodProvider>
