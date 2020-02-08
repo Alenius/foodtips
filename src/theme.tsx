76a4477e5c9d89ffbd54e9fa171c634f83d63ec4
Adam Alenius <adam.alenius@gmail.com>
2020-02-08 09:47:48 +0100

    style: add rems and ems to the theme object

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,5 +1,26 @@
 export default {
-  primaryBackgroundColor: '#282c34',
-  secondaryBackgroundColor: '#454754',
-  lightgreen: '#8cd881'
+  color: {
+    primaryBackgroundColor: '#282c34',
+    secondaryBackgroundColor: '#454754',
+    lightgreen: '#8cd881',
+  },
+  // since the body has font-size of 62.5%
+  // the base font is 10px large. This makes
+  // it easy to calculate based on rems and ems.
+  rem: {
+    xs: '1rem',
+    s: '1.3rem',
+    m: '1.6rem',
+    l: '2rem',
+    xl: '2.5rem',
+    xxl: '3.rem',
+  },
+  em: {
+    xs: '1em',
+    s: '1.3em',
+    m: '1.6em',
+    l: '2em',
+    xl: '2.5em',
+    xxl: '3.em',
+  },
 };
