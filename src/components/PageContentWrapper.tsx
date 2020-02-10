71231cc63388c7ed76d31d2f3fa8f95e9899fb46
Adam Alenius <adam.alenius@gmail.com>
2020-02-10 07:40:00 +0100

    refactor: remove the animation wrapper and merge its features with PageContentWrapper

--- src/components/PageContentWrapper.tsx
+++ src/components/PageContentWrapper.tsx
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
+import { useSpring, animated } from 'react-spring';
 import theme from 'theme';
 
-const PageContentWrapper: React.FC = ({ children }) => {
-  const Wrapper = styled.div`
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-between;
-    background-color: ${theme.color.primaryBackgroundColor};
-    padding: ${theme.em.xl} ${theme.rem.xl};
-  `;
+const MainWrapper = styled.div`
+  display: flex;
+  padding: ${theme.em.xl} ${theme.rem.xl};
+  flex: 1;
+  background-color: ${theme.color.secondaryBackgroundColor};
+`;
 
-  return <Wrapper>{children}</Wrapper>;
+const AnimatedWrapper = styled(animated.div)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 1;
+`;
+
+interface Props {
+  backgroundColor?: string;
+}
+
+const PageContentWrapper: React.FC<Props> = ({ children }) => {
+  const [props, set] = useSpring(() => ({ opacity: 0 }));
+
+  useEffect(() => {
+    set({ opacity: 1 });
+  }, [set]);
+
+  // the animated div is needed so that the background does not flash white when the opacity
+  // shift is happening
+  return (
+    <MainWrapper>
+      <AnimatedWrapper style={props}>{children}</AnimatedWrapper>
+    </MainWrapper>
+  );
 };
 
 export default PageContentWrapper;
